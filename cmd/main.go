46e1b221c6c3ac805545e02ea164877e14ae243f
pilyang <pilyang.dev@gmail.com>
2025-01-17 21:46:16 +0900

    refactor: db 설정 분리

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
-	"os"
 
 	"github.com/techbloghub/server/config"
-	"github.com/techbloghub/server/ent"
+	"github.com/techbloghub/server/internal/database"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
@@ -19,27 +17,16 @@ func main() {
 		log.Fatalf("failed to load config: %v", cfgErr)
 	}
 
-	pgCfg := cfg.PostgresConfig
-	serverCfg := cfg.ServerConfig
-
-	client, errPg := ent.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable",
-		pgCfg.Host, pgCfg.Port, pgCfg.User, pgCfg.Db, pgCfg.Password))
+	// DB 연결
+	client, errPg := database.ConnectDatabase(cfg)
 	if errPg != nil {
-		log.Fatalf("failed opening connection to postgres: %v", errPg)
+		log.Fatalf("failed to connect database: %v", errPg)
 	}
 	defer client.Close()
 
-	// Run the auto migration tool.
-	if serverCfg.Env == "local" {
-		if err := client.Schema.Create(context.Background()); err != nil {
-			log.Fatalf("failed creating schema resources: %v", err)
-		}
-	}
-
-	// PORT 환경변수에서 가져오기
-	// 후에 이런 config값 관리할것들 많아지면 후에 Config struct등으로 분리 고려
+	// 서버 실행
 	r := setRouter()
-	routerErr := r.Run(":" + serverCfg.Port)
+	routerErr := r.Run(":" + cfg.ServerConfig.Port)
 	if routerErr != nil {
 		fmt.Println("Error while running server: ", cfgErr)
 		return
@@ -52,13 +39,4 @@ func setRouter() *gin.Engine {
 		context.String(200, "pong")
 	})
 	return r
-}
-
-func getEnvWithDefault(key, defaultValue string) string {
-	if value := os.Getenv(key); value != "" {
-		fmt.Println("env get success", key, value)
-		return value
-	}
-	fmt.Println("return default value for ", key)
-	return defaultValue
 }
