1bacbee648a79f54bdf8af910143f8d369891e58
pilyang <pilyang.dev@gmail.com>
2025-01-25 19:18:21 +0900

    test: transactional test 유틸 함수 작성

--- internal/testutils/db.go
+++ internal/testutils/db.go
@@ -36,3 +36,22 @@ func TearDown(client *ent.Client, tx *ent.Tx) {
 	tx.Rollback()
 	client.Close()
 }
+
+/**
+* TransactionalTest는 트랜잭션을 사용하여 테스트를 실행하는 함수입니다.
+* 테스트가 종료된 후 트랜잭션을 롤백하여 데이터베이스 상태를 원래대로 복원합니다.
+*
+* @param t *testing.T - 테스트 핸들러
+* @param fn func(t *testing.T, client *ent.Client) - 테스트 함수, ent.Client를 인자로 받습니다.
+ */
+func TransactionalTest(t *testing.T, fn func(t *testing.T, client *ent.Client)) {
+	client, tx := SetupDB(t)
+	defer func() {
+		if err := tx.Rollback(); err != nil {
+			t.Fatalf("트랜잭션 롤백 실패: %v", err)
+		}
+		client.Close()
+	}()
+
+	fn(t, tx.Client())
+}
