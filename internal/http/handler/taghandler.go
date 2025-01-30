f2c9d0ba2b789a5b88043a268d3656421712b0bf
iamjooon2 <iamjooon2@gmail.com>
2025-01-30 15:38:57 +0900

    refactor: 중복제거

--- internal/http/handler/taghandler.go
+++ internal/http/handler/taghandler.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/techbloghub/server/ent"
+	"github.com/techbloghub/server/ent/tag"
 )
 
 type TagResponse struct {
@@ -17,24 +18,43 @@ type TagResponses struct {
 
 func GetTags(client *ent.Client) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		tagEntities, err := client.Tag.Query().All(c)
+		searchParam := c.DefaultQuery("search", "")
+
+		var tags []*ent.Tag
+		var err error
+
+		if searchParam != "" {
+			tags, err = searchTagsByName(c, client, searchParam)
+		} else {
+			tags, err = client.Tag.Query().All(c)
+		}
+
 		if err != nil {
-			c.JSON(500, gin.H{"error": err})
+			c.JSON(500, gin.H{"error": err.Error()})
 			return
 		}
 
-		tagCounts := len(tagEntities)
-		tagResponses := make([]TagResponse, tagCounts)
-		for i, tag := range tagEntities {
-			tagResponses[i] = TagResponse{
-				ID:   tag.ID,
-				Name: tag.Name,
-			}
-		}
-
+		tagResponses := makeTagResponses(tags)
 		c.JSON(200, TagResponses{
-			Count: tagCounts,
+			Count: len(tagResponses),
 			Tags:  tagResponses,
 		})
 	}
+}
+
+func searchTagsByName(c *gin.Context, client *ent.Client, searchParam string) ([]*ent.Tag, error) {
+	return client.Tag.Query().
+		Where(tag.NameContains(searchParam)).
+		All(c)
+}
+
+func makeTagResponses(tags []*ent.Tag) []TagResponse {
+	tagResponses := make([]TagResponse, len(tags))
+	for i, tag := range tags {
+		tagResponses[i] = TagResponse{
+			ID:   tag.ID,
+			Name: tag.Name,
+		}
+	}
+	return tagResponses
 }
