3c303342068985580647d9406a84ac83d3acd04a
iamjooon2 <iamjooon2@gmail.com>
2025-03-02 14:06:59 +0900

    refactor: 메서드 분리

--- internal/http/handler/postinghandler.go
+++ internal/http/handler/postinghandler.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -30,60 +31,65 @@ type PostingSearchResponses struct {
 func GetPostings(client *ent.Client) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		titleSearchParam := c.DefaultQuery("title", "")
-		paging := common.GenerateTechPaging(
-			c.Query("cursor"),
-			c.Query("size"),
-		)
+		paging := common.GenerateTechPaging(c.Query("cursor"), c.Query("size"))
 
-		query := client.Posting.
-			Query().
-			WithCompany()
-		if titleSearchParam != "" {
-			query = query.Where(posting.TitleContainsFold(titleSearchParam))
-		}
-		if paging.Cursor > 0 {
-			query = query.Where(posting.IDLT(paging.Cursor))
-		}
-
-		postings, err := query.
-			Order(
-				ent.Desc(posting.FieldPublishedAt),
-				ent.Desc(posting.FieldID),
-			).
-			Limit(paging.Size).
-			All(c)
+		totalCount, err := countTotalPostings(client, titleSearchParam, c)
 		if err != nil {
-			c.JSON(500, gin.H{"error": err.Error()})
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
 		}
 
-		postingsByTitle := make([]TitleSearchResponse, len(postings))
-		for i, posting := range postings {
-			postingsByTitle[i] = TitleSearchResponse{
-				ID:            posting.ID,
-				Title:         posting.Title,
-				Url:           posting.URL.String(),
-				Company:       posting.Edges.Company.Name,
-				Logo:          posting.Edges.Company.LogoURL.String(),
-				Tags:          posting.Tags.ToStringSlice(),
-				CreateTime:    posting.CreateTime,
-				UpdateTime:    posting.UpdateTime,
-				PublishedTime: posting.PublishedAt,
-			}
-		}
-
-		totalCount, err := client.Posting.Query().
-			Where(posting.TitleContainsFold(titleSearchParam)).
-			Count(c)
+		postings, err := fetchPostings(client, titleSearchParam, paging, c)
 		if err != nil {
-			c.JSON(500, gin.H{"error": err.Error()})
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
 		}
 
-		c.JSON(200, PostingSearchResponses{
+		c.JSON(http.StatusOK, PostingSearchResponses{
 			Count:       totalCount,
-			Postings:    postingsByTitle,
+			Postings:    convertToTitleSearchResponse(postings),
 			HasNextPage: paging.HasNextPage(totalCount),
 		})
 	}
+}
+
+func fetchPostings(client *ent.Client, title string, paging common.TechbloghubPaging, c *gin.Context) ([]*ent.Posting, error) {
+	query := client.Posting.Query().WithCompany()
+	if title != "" {
+		query = query.Where(posting.TitleContainsFold(title))
+	}
+	if paging.Cursor > 0 {
+		query = query.Where(posting.IDLT(paging.Cursor))
+	}
+
+	return query.Order(
+		ent.Desc(posting.FieldPublishedAt),
+		ent.Desc(posting.FieldID),
+	).Limit(paging.Size).All(c)
+}
+
+func countTotalPostings(client *ent.Client, title string, c *gin.Context) (int, error) {
+	query := client.Posting.Query()
+	if title != "" {
+		query = query.Where(posting.TitleContainsFold(title))
+	}
+	return query.Count(c)
+}
+
+func convertToTitleSearchResponse(postings []*ent.Posting) []TitleSearchResponse {
+	responses := make([]TitleSearchResponse, len(postings))
+	for i, posting := range postings {
+		responses[i] = TitleSearchResponse{
+			ID:            posting.ID,
+			Title:         posting.Title,
+			Url:           posting.URL.String(),
+			Company:       posting.Edges.Company.Name,
+			Logo:          posting.Edges.Company.LogoURL.String(),
+			Tags:          posting.Tags.ToStringSlice(),
+			CreateTime:    posting.CreateTime,
+			UpdateTime:    posting.UpdateTime,
+			PublishedTime: posting.PublishedAt,
+		}
+	}
+	return responses
 }
