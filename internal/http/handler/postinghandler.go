7dd79dab045783d147c0d71817cb65f9248a9945
iamjooon2 <iamjooon2@gmail.com>
2025-03-03 23:36:12 +0900

    feat: 태그 검색 기능 추가

--- internal/http/handler/postinghandler.go
+++ internal/http/handler/postinghandler.go
@@ -1,14 +1,15 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 	"strings"
 	"time"
 
 	"entgo.io/ent/dialect/sql"
-	"entgo.io/ent/dialect/sql/sqljson"
 	"github.com/gin-gonic/gin"
 	"github.com/techbloghub/server/ent"
+	"github.com/techbloghub/server/ent/company"
 	"github.com/techbloghub/server/ent/posting"
 	"github.com/techbloghub/server/internal/common"
 )
@@ -31,6 +32,7 @@ type PostingSearchResponses struct {
 	HasNextPage bool                    `json:"has_next_page"`
 }
 
+// ✅ GetPostings을 더 깔끔하게 정리
 func GetPostings(client *ent.Client) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		titleSearchParam := c.DefaultQuery("title", "")
@@ -38,108 +40,110 @@ func GetPostings(client *ent.Client) gin.HandlerFunc {
 
 		paging := common.GenerateTechPaging(c.Query("cursor"), c.Query("size"))
 
-		if titleSearchParam == "" && tagsSearchParam == "" {
-			posts, err := fetchPostings(client, "", paging, c)
-			totalCount, err := countTotalPostings(client, "", c)
-			if err != nil {
-				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-				return
-			}
-
-			c.JSON(http.StatusOK, PostingSearchResponses{
-				Count:       totalCount,
-				Postings:    convertToTitleSearchResponse(posts),
-				HasNextPage: paging.HasNextPage(totalCount),
-			})
-			return
-		}
-
-		if titleSearchParam != "" {
-			counts, err := countTotalPostings(client, titleSearchParam, c)
-			if err != nil {
-				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-				return
-			}
-			postings, err := fetchPostings(client, titleSearchParam, paging, c)
-			if err != nil {
-				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-				return
-			}
-
-			c.JSON(http.StatusOK, PostingSearchResponses{
-				Count:       counts,
-				Postings:    convertToTitleSearchResponse(postings),
-				HasNextPage: paging.HasNextPage(counts),
-			})
-		} else {
-			totalCount, err := countTotalPostings(client, tagsSearchParam, c)
-			if err != nil {
-				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-				return
-			}
-
-			postings, err := fetchPostings(client, tagsSearchParam, paging, c)
-			if err != nil {
-				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-				return
-			}
-
-			c.JSON(http.StatusOK, PostingSearchResponses{
-				Count:       totalCount,
-				Postings:    convertToTitleSearchResponse(postings),
-				HasNextPage: paging.HasNextPage(totalCount),
-			})
-		}
+		handlePostingsQuery(client, titleSearchParam, tagsSearchParam, paging, c)
 	}
+}
+
+func handlePostingsQuery(client *ent.Client, title, tags string, paging common.TechbloghubPaging, c *gin.Context) {
+	searchParam := title
+	if searchParam == "" {
+		searchParam = tags
+	}
+
+	postings, err := fetchPostings(client, searchParam, paging, c)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	totalCount, err := countPostings(client, searchParam, c)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, PostingSearchResponses{
+		Count:       totalCount,
+		Postings:    convertToTitleSearchResponse(postings),
+		HasNextPage: paging.HasNextPage(totalCount),
+	})
 }
 
 func fetchPostings(client *ent.Client, param string, paging common.TechbloghubPaging, c *gin.Context) ([]*ent.Posting, error) {
-	query := client.Posting.Query().WithCompany()
-	if strings.Contains(param, ",") {
-		arr := strings.Split(param, ",")
+	query := client.Posting.Query()
+
+	if param != "" {
+		query = applySearchFilter(query, param)
+	}
+
+	if paging.Cursor > common.CURSOR_DEFAULT {
+		query = query.Where(posting.IDLT(paging.Cursor))
+	}
+	return query.Where(posting.HasCompany()).
+		WithCompany(func(q *ent.CompanyQuery) {
+			q.Select(
+				company.FieldLogoURL,
+				company.FieldName,
+			)
+		}).
+		Order(
+			ent.Desc(posting.FieldID),
+			ent.Desc(posting.FieldID),
+		).
+		Limit(paging.Size).
+		All(c.Request.Context())
+}
+
+func applySearchFilter(query *ent.PostingQuery, param string) *ent.PostingQuery {
+	// before: [react,java]
+	// after: ARRAY[react,java]
+	if strings.HasPrefix(param, "[") && strings.HasSuffix(param, "]") {
+		arrayQuery := "ARRAY" + param
+
 		query = query.Where(func(s *sql.Selector) {
-			s.Where(sqljson.ValueContains("tags", arr))
+			s.Where(sql.ExprP(fmt.Sprintf("%s @> %s", s.C("tags"), arrayQuery)))
 		})
+		// 단일 제목으로 검색하는 케이스는 제목 검색
 	} else if param != "" {
 		query = query.Where(posting.TitleContainsFold(param))
 	}
-	if paging.Cursor > 0 {
-		query = query.Where(posting.IDLT(paging.Cursor))
-	}
-
-	return query.Order(
-		ent.Desc(posting.FieldPublishedAt),
-		ent.Desc(posting.FieldID),
-	).Limit(paging.Size).All(c)
+	return query
 }
 
-func countTotalPostings(client *ent.Client, param string, c *gin.Context) (int, error) {
+func countPostings(client *ent.Client, param string, c *gin.Context) (int, error) {
 	query := client.Posting.Query()
 	if param != "" {
-		query = query.Where(posting.TitleContainsFold(param))
-	} else if strings.Contains(param, ",") {
-		arr := strings.Split(param, ",")
-		query = query.Where(func(s *sql.Selector) {
-			s.Where(sqljson.ValueContains("tags", arr))
-		})
+		query = applySearchFilter(query, param)
 	}
 	return query.Count(c)
 }
 
+// 대체 왜 못갖고오냐
+func getCompanyInfo(posting *ent.Posting) (string, string) {
+	if posting.Edges.Company != nil {
+		return posting.Edges.Company.Name, posting.Edges.Company.LogoURL.String()
+	}
+	return "Unknown", ""
+}
+
 func convertToTitleSearchResponse(postings []*ent.Posting) []PostingSearchResponse {
-	responses := make([]PostingSearchResponse, len(postings))
-	for i, posting := range postings {
-		responses[i] = PostingSearchResponse{
+	var responses []PostingSearchResponse
+
+	for _, posting := range postings {
+		companyName, logoURL := getCompanyInfo(posting)
+
+		responses = append(responses, PostingSearchResponse{
 			ID:            posting.ID,
 			Title:         posting.Title,
 			Url:           posting.URL.String(),
-			Company:       posting.Edges.Company.Name,
-			Logo:          posting.Edges.Company.LogoURL.String(),
+			Company:       companyName,
+			Logo:          logoURL,
 			Tags:          posting.Tags.ToStringSlice(),
 			CreateTime:    posting.CreateTime,
 			UpdateTime:    posting.UpdateTime,
 			PublishedTime: posting.PublishedAt,
-		}
+		})
 	}
+
 	return responses
 }
